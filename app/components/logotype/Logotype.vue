52f14c6c13bf0314a918265d362ff404671ff734
Oliver Elfverson <oliver@zyner.org>
2026-08-25 16:54:10 +0200

    Fix fonts

--- app/components/logotype/Logotype.vue
+++ app/components/logotype/Logotype.vue
@@ -7,6 +7,6 @@ defineProps({
 <template>
   <a class="flex items-center gap-2" href="/">
     <img src="https://s3.zyner.org/brand/logo.svg" class="size-10 rounded-lg" />
-    <h1 v-if="title" class="font-bold text-xl">Zyner</h1>
+    <h1 v-if="title" class="font-bold text-xl font-mono">Zyner</h1>
   </a>
 </template>
