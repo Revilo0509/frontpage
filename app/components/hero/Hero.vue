dfa6869f8a57a704f9f493a16238948cffa63ff1
Oliver Elfverson <oliver@zyner.org>
2026-09-20 21:15:31 +0200

    Add hero

---
+++ app/components/hero/Hero.vue
@@ -0,0 +1,9 @@
+<template>
+  <section class="relative">
+    <img
+      src="/hero.webp"
+      class="block w-full h-[calc(100dvh-var(--navbar-height))] object-cover object-top"
+    />
+    <div class="absolute inset-0 bg-linear-to-r from-accent-950 to-default/50"></div>
+  </section>
+</template>
